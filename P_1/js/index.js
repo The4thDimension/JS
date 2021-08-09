1a0006d0a4eb902410f3588e56149e5d80efd843
Mahesh <pmaheshiyer@gmail.com>
2021-08-09 10:32:19 +0530

    Update

--- P_1/js/index.js
+++ P_1/js/index.js
@@ -10,3 +10,36 @@ function change()
         choice  = "Are you sure you want to cancel?";
     alert(choice);
 }
+
+
+function changeColor()
+{
+    f1 = document.getElementById("first");
+    f2 = document.getElementById("second");
+    f1.className = "cl1";
+    f2.className = "cl2";
+}
+
+
+function changeText()
+{
+    f1 = document.getElementById("first");
+    f2 = document.getElementById("second");
+    f1.innerHTML = "Changed text";
+    f2.innerHTML = "Again changed text";
+}
+
+
+function changeColorQuick()
+{
+    document.getElementById("first").style.background = "yellow";
+    document.getElementById("first").style.color = "blue";
+    document.getElementById("second").style.background = "blue";
+    document.getElementById("second").style.color = "yellow";
+}
+
+
+function changeValue()
+{
+    document.getElementById("ch").value = "New Value";
+}
