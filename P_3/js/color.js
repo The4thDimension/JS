b6abb585ea97175d88959ab993cc40a6be15fd35
Mahesh <pmaheshiyer@gmail.com>
2021-08-10 11:47:13 +0530

    Color change using javascript

---
+++ P_3/js/color.js
@@ -0,0 +1,9 @@
+function makeLime(){
+    d = document.getElementById("getThis");
+    d.style.backgroundColor = "Lime";
+}
+
+function makeYellow(){
+    d = document.getElementById("getThis");
+    d.style.backgroundColor = "Yellow";
+}
