fc62e450008898d7ced4db850dab57d6b142a6e5
Mahesh <pmaheshiyer@gmail.com>
2021-08-22 17:52:32 +0530

    Reset attribute'

--- P_10_Filter/js/script.js
+++ P_10_Filter/js/script.js
@@ -1,23 +1,25 @@
 canvas = document.getElementById("can");
 var image = null;
+var imageGrey = null;
+var imageRed = null;
 
 function uploadImage(){
   
-  var img = document.getElementById("img");
+  img = document.getElementById("img");
   image = new SimpleImage(img);
+  imageGrey = new SimpleImage(img);
+  imageRed = new SimpleImage(img);
   image.drawTo(canvas);
   
 }
 
 function doGrey(){
   
-  if(image == null)
+  if(imageGrey == null)
     alert("No Image");
   else{
-    imageGrey = image;
     for(var pixel of imageGrey.values()){
-      var tot = pixel.getRed() + pixel.getGreen() + pixel.getBlue();
-      var avg = tot / 3;
+      var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
       pixel.setRed(avg);
       pixel.setGreen(avg);
       pixel.setBlue(avg);
@@ -25,5 +27,29 @@ function doGrey(){
       ctx.clearRect(0,0,canvas.width,canvas.height);
       imageGrey.drawTo(canvas);
     }
+  }
+}
+
+function doRed(){
+
+  if(imageRed == null)
+    alert("No Image");
+  else{
+    for(var pixel of imageRed.values()){
+      pixel.setRed(255);
+      var ctx = canvas.getContext("2d");
+      ctx.clearRect(0,0,canvas.width,canvas.height);
+      imageRed.drawTo(canvas);
+    }
+  }
+}
+
+function doReset(){
+  if(image == null)
+    alert("No Image");
+  else{
+    var ctx = canvas.getContext("2d");
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    image.drawTo(canvas);
   }
 }
