0585891e79c98ca77661a5a94b39f3ead86695f7
Mahesh <pmaheshiyer@gmail.com>
2021-08-23 08:03:58 +0530

    Blur attribute

--- P_10_Filter/js/script.js
+++ P_10_Filter/js/script.js
@@ -2,6 +2,7 @@ canvas = document.getElementById("can");
 var image = null;
 var imageGrey = null;
 var imageRed = null;
+var imageBlur = null;
 
 function uploadImage(){
   
@@ -9,6 +10,7 @@ function uploadImage(){
   image = new SimpleImage(img);
   imageGrey = new SimpleImage(img);
   imageRed = new SimpleImage(img);
+  imageBlur = new SimpleImage(img);
   image.drawTo(canvas);
   
 }
@@ -45,11 +47,58 @@ function doRed(){
 }
 
 function doReset(){
+
   if(image == null)
     alert("No Image");
   else{
     var ctx = canvas.getContext("2d");
     ctx.clearRect(0,0,canvas.width,canvas.height);
     image.drawTo(canvas);
+  }
+
+}
+
+function doBlur(){
+
+  if(imageBlur == null)
+    alert("No image");
+  else{
+    var red = 0;
+    var green = 0;
+    var blue = 0;
+    var xCount = 0;
+    var yCount = 0;
+    var height = imageBlur.getHeight();
+    var width = imageBlur.getWidth();
+    for(var y = 0;y <= height;y = y + 3){
+      yCount = y;
+      for(var x = 0;x <= width;x = x + 3){
+        xCount = x;
+        for(var i = 0;i < 3;i++){
+          for(var j = 0;j < 3;j++){
+              var pixel = imageBlur.getPixel(xCount,yCount);
+              red = red + pixel.getRed();
+              blue = blue + pixel.getBlue();
+              green = green + pixel.getGreen();
+              yCount = yCount + 1;
+          }
+          xCount = xCount + 1;
+          yCount = y;
+        }
+        xCount = x;
+        yCount = y;
+        for(var i = 0;i < 3;i++){
+          for(var j = 0;j < 3;j++){
+            var pixel = imageBlur.getPixel(xCount,yCount);
+            pixel.setRed(red / 9);
+            pixel.setBlue(blue / 9);
+            pixel.setGreen(green / 9);
+            yCount = yCount + 1;
+          }
+          xCount = xCount + 1;
+          yCount = y;
+        }
+      }
+    }
   }
 }
