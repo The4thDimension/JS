f8238f4d81e5cb144670faec9618f4c099a06cb6
Mahesh <pmaheshiyer@gmail.com>
2021-09-03 09:37:42 +0530

    Blue value bug fix

--- P_11_steganography/js/script.js
+++ P_11_steganography/js/script.js
@@ -1,5 +1,6 @@
 var image1 = null;
 var image2 = null;
+var image3 = null;
 var canvas1 = document.getElementById("can1");
 var canvas2 = document.getElementById("can2");
 var canvas3 = document.getElementById("can3");
@@ -29,6 +30,50 @@ function splitPixel(px){
 
 }
 
+/*function combineImage(){
+
+    if(image1 == null)
+        alert("Please load the image to alter...");
+    else if(image2 == null)
+        alert("Please load the image to hide...");
+    else{
+
+        var h1 = image1.getHeight();
+        var h2 = image2.getHeight();
+        var w1 = image1.getWidth();
+        var w2 = image2.getWidth();
+        var height;
+        var width;
+        if(h1 < h2)
+            height = h1;
+        else
+            height = h2;
+        if(w1 < w2)
+            width = w1;
+        else
+            width = w2;
+        var image3 = new SimpleImage(width,height);
+
+        for(var newPixel of image3.values()){
+
+            var x = newPixel.getX();
+            var y = newPixel.getY();
+            var pixel = image1.getPixel(x,y);
+            var hidePixel = image2.getPixel(x,y);
+            splitPixel(pixel);
+            splitPixel(hidePixel);
+            newPixel.setRed(pixel.getRed() + (hidePixel.getRed() / 16));
+            newPixel.setGreen(pixel.getGreen() + (hidePixel.getGreen() / 16));
+            newPixel.setRed(pixel.getBlue() + (hidePixel.getBlue() / 16));
+
+        }
+
+        image3.drawTo(canvas3);
+
+    }
+
+}*/
+
 function combineImage(){
 
     if(image1 == null)
@@ -49,9 +94,9 @@ function combineImage(){
             }
             splitPixel(pixel);
             splitPixel(hidePixel);
-            pixel.setRed(pixel.getRed() + (hidePixel.getRed() / 16))
-            pixel.setGreen(pixel.getGreen() + (hidePixel.getGreen() / 16))
-            pixel.setRed(pixel.getBlue() + (hidePixel.getBlue() / 16))
+            pixel.setRed(pixel.getRed() + (hidePixel.getRed() / 16));
+            pixel.setGreen(pixel.getGreen() + (hidePixel.getGreen() / 16));
+            pixel.setBlue(pixel.getBlue() + (hidePixel.getBlue() / 16));
 
         }
 
@@ -60,6 +105,7 @@ function combineImage(){
     }
 
 }
+
 
 function getSplit(px){
 
