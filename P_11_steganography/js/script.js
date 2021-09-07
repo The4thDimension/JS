b58414e8dfc4c6f007c904443c75ffe07269eb08
Mahesh <pmaheshiyer@gmail.com>
2021-09-07 07:02:22 +0530

    Crop

--- P_11_steganography/js/script.js
+++ P_11_steganography/js/script.js
@@ -1,15 +1,70 @@
 var image1 = null;
 var image2 = null;
+var image3 = null;
 var canvas1 = document.getElementById("can1");
 var canvas2 = document.getElementById("can2");
 var canvas3 = document.getElementById("can3");
 var canvas4 = document.getElementById("can4");
+
+function crop(newImage,width,height){
+
+    var returnImage = new SimpleImage(width,height);
+    for(var px of returnImage.pixels()){
+
+        var x = px.getX();
+        var y = px.getY();
+
+        try{
+            var changePixel = newImage.getPixel(x,y);
+        }
+
+        catch{
+            continue;
+        }
+
+        px.setRed(changePixel.getRed());
+        px.setGreen(changePixel.getGreen());
+        px.setBlue(changePixel.getBlue());
+        
+    }
+
+    return returnImage;
+
+}
+
+function cropImage(image1,image2){
+
+    var h1 = image1.getHeight();
+    var h2 = image2.getHeight();
+    if(h1 < h2){
+
+        var width = image1.getWidth();
+        alert(h1);
+        alert(width);
+        //return crop(image2,width,h1);
+
+    }
+
+    else{
+
+        var width = image2.getWidth();
+        alert(h2);
+        alert(width);
+        //return crop(image1,width,h2);
+
+    }
+
+}
 
 function uploadImage1(){
 
     var img = document.getElementById("img1");
     image1 = new SimpleImage(img);
     image1.drawTo(canvas1);
+    if(image2 != null){
+        image3 = cropImage(image1,image2);
+        //image3.drawTo(canvas3);
+    }
 
 }
 
@@ -18,6 +73,10 @@ function uploadImage2(){
     var img = document.getElementById("img2");
     image2 = new SimpleImage(img);
     image2.drawTo(canvas2);
+    if(image1 != null){
+        image3 = cropImage(image1,image2);
+        //image3.drawTo(canvas3);
+    }
     
 }
 
