b5148f55647d93e4a39613f683d13fbff3b2b2f1
Mahesh <pmaheshiyer@gmail.com>
2021-09-21 18:51:36 +0530

    Crop

--- P_11_steganography/js/script.js
+++ P_11_steganography/js/script.js
@@ -1,6 +1,7 @@
 var image1 = null;
 var image2 = null;
-var image3 = null;
+var Image1 = null;
+var Image2 = null;
 var canvas1 = document.getElementById("can1");
 var canvas2 = document.getElementById("can2");
 var canvas3 = document.getElementById("can3");
@@ -32,60 +33,12 @@ function crop(newImage,width,height){
 
 }
 
-/*function cropImage(image1,image2){
-
-    var h1 = image1.getHeight();
-    var h2 = image2.getHeight();
-    if(h1 < h2){
-
-        var width = image1.getWidth();
-        alert(h1);
-        alert(width);
-        //return crop(image2,width,h1);
-
-    }
-
-    else{
-
-        var width = image2.getWidth();
-        alert(h2);
-        alert(width);
-        //return crop(image1,width,h2);
-
-    }
-
-}*/
 
 function uploadImage1(){
 
     var img = document.getElementById("img1");
     image1 = new SimpleImage(img);
     image1.drawTo(canvas1);
-    if(image2 != null){
-
-        var h1 = image1.getHeight();
-        var h2 = image2.getHeight();
-        if(h1 < h2){
-
-            var width = image1.getWidth();
-            alert(h1);
-            alert(width);
-            //return crop(image2,width,h1);
-
-        }
-
-        else{
-
-            var width = image2.getWidth();
-            alert(h2);
-            alert(width);
-            //return crop(image1,width,h2);
-
-        }
-
-        //image3.drawTo(canvas3);
-
-    }
 
 }
 
@@ -94,29 +47,6 @@ function uploadImage2(){
     var img = document.getElementById("img2");
     image2 = new SimpleImage(img);
     image2.drawTo(canvas2);
-    if(image1 != null){
-        if(image1.compl)
-        var h1 = image1.getHeight();
-        var h2 = image2.getHeight();
-        if(h1 < h2){
-
-            var width = image1.getWidth();
-            alert(parseInt(h1));
-            alert(parseInt(width));
-            //return crop(image2,width,h1);
-
-        }
-
-        else{
-
-            var width = image2.getWidth();
-            alert(h2);
-            alert(width);
-            //return crop(image1,width,h2);
-
-        }
-        //image3.drawTo(canvas3);
-    }
     
 }
 
@@ -128,64 +58,35 @@ function splitPixel(px){
 
 }
 
-/*function combineImage(){
-
+function combineImage(){
     if(image1 == null)
-        alert("Please load the image to alter...");
+    alert("Please load the image to alter...");
     else if(image2 == null)
-        alert("Please load the image to hide...");
+    alert("Please load the image to hide...");
+
     else{
 
         var h1 = image1.getHeight();
         var h2 = image2.getHeight();
-        var w1 = image1.getWidth();
-        var w2 = image2.getWidth();
         var height;
         var width;
-        if(h1 < h2)
+        if(h1 < h2){
             height = h1;
-        else
-            height = h2;
-        if(w1 < w2)
-            width = w1;
-        else
-            width = w2;
-        var image3 = new SimpleImage(width,height);
-
-        for(var newPixel of image3.values()){
-
-            var x = newPixel.getX();
-            var y = newPixel.getY();
-            var pixel = image1.getPixel(x,y);
-            var hidePixel = image2.getPixel(x,y);
-            splitPixel(pixel);
-            splitPixel(hidePixel);
-            newPixel.setRed(pixel.getRed() + (hidePixel.getRed() / 16));
-            newPixel.setGreen(pixel.getGreen() + (hidePixel.getGreen() / 16));
-            newPixel.setRed(pixel.getBlue() + (hidePixel.getBlue() / 16));
-
+            width = image1.getWidth();
         }
+        else{
+            height = h2;
+            width = image2.getWidth();
+        }  
+        Image1 = crop(image1,width,height);
+        Image2 = crop(image2,width,height)  
 
-        image3.drawTo(canvas3);
-
-    }
-
-}*/
-
-function combineImage(){
-
-    if(image1 == null)
-        alert("Please load the image to alter...");
-    else if(image2 == null)
-        alert("Please load the image to hide...");
-    else{
-
-        for(var pixel of image1.values()){
+        for(var pixel of Image1.values()){
 
             var x = pixel.getX();
             var y = pixel.getY();
             try{
-                var hidePixel = image2.getPixel(x,y);
+                var hidePixel = Image2.getPixel(x,y);
             }
             catch(err){
                 continue;
@@ -198,7 +99,7 @@ function combineImage(){
 
         }
 
-        image1.drawTo(canvas3);
+        Image1.drawTo(canvas3);
 
     }
 
@@ -221,13 +122,13 @@ function getImage(){
         alert("Please load the image to hide...");
     else{
 
-        for(var pixel of image1.values()){
+        for(var pixel of Image1.values()){
 
             getSplit(pixel);
 
         }
 
-        image1.drawTo(canvas4);
+        Image1.drawTo(canvas4);
 
     }
 
